57ebd710fb95f7156cb55ae2234df95e7385f536
Clive Da <cliveda@example.com>
2020-01-16 21:42:00 +0000

    autocommit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-aviationstack
+
 node-aviationstack
+
+run with
+```
+NODE_PATH=/usr/local/lib/node_modules MYAPIKEY=123 node .
+```
+
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ axios.get('https://api.aviationstack.com/v1/flights', {params})
     if (Array.isArray(apiResponse['results'])) {
         apiResponse['results'].forEach(flight => {
             if (!flight['live']['is_ground']) {
-                console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
+                console.table(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
                     `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
                     `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
             }
